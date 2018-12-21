819a6ee3038be18a4a2ba7c66fb982eab8e6ea49
naebem99 <36677311+naebem99@users.noreply.github.com>
2018-12-21 16:04:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,51 +1,72 @@
+#!/usr/bin/env python
 import requests
 import telebot
 import time
 import urllib
-VK = 'https://api.vk.com/method/wall.get?v=5.92&domain=spb_today&fields=all&offset=2&count=1&filter=owner&access_token='
-post_id = open('post_id.txt', 'r') #Последний пост
-global id_last_post
-id_last_post = post_id.read()
-bot = telebot.TeleBot('')
-CHANNEL_NAME = '@'
+#local
+VK = 'https://api.vk.com/method/wall.get?v=5.92&domain=spb_today&fields=all&offset=1&count=1&filter=owner&access_token=TOKEN'
+#server
+#VK = 'https://api.vk.com/method/wall.get?v=5.92&domain=spb_today&fields=all&offset=1&count=1&filter=owner&access_token=TOKEN'
+bot = telebot.TeleBot('TOKEN')
+CHANNEL_NAME = '@CHANEL'
 
 
 def main():
     try:
         feed = requests.get(VK)
         json_data = feed.json()
-        global id_last
+        #global id_last
         id_last = json_data['response']['items'][0]['id'] #id
         print(id_last)
-        global text
+        #global text
         text = json_data['response']['items'][0]['text'] #text
-        global url_photo
-        url_photo = json_data['response']['items'][0]['attachments'][0]['photo']['sizes'][4]['url']#photo_url
-        print(url_photo)
-        if str(id_last) > str(id_last_post):
-            ph = open('out.jpg', 'wb')
-            ph.write(urllib.request.urlopen(url_photo).read())
-            ph.close()
-            img = open('out.jpg', 'rb')
-            bot.send_chat_action(CHANNEL_NAME, 'upload_photo')
-            bot.send_photo(CHANNEL_NAME, img)
-            img.close()
-            bot.send_message(CHANNEL_NAME, text)
-            post_id = open('post_id.txt', 'w')
-            post_id.write(str(id_last))
-            post_id.close()
-            time.sleep(30)
-            main()
+        def send():
+            post_id = open('post_id.txt', 'r')  # Последний пост
+            global id_last_post
+            id_last_post = post_id.read()
+            if str(id_last) > str(id_last_post):
+                ph = open('out.jpg', 'wb')
+                ph.write(urllib.request.urlopen(url_photo).read())
+                ph.close()
+                img = open('out.jpg', 'rb')
+                bot.send_chat_action(CHANNEL_NAME, 'upload_photo')
+                bot.send_photo(CHANNEL_NAME, img)
+                img.close()
+                bot.send_message(CHANNEL_NAME, text)
+                post_id = open('post_id.txt', 'w')
+                post_id.write(str(id_last))
+                img = open('out.jpg', 'w')
+                img.close()
+                post_id.close()
+
+                time.sleep(30)
+                main()
+        try:
+            global url_photo
+            url_photo = json_data['response']['items'][0]['attachments'][0]['photo']['sizes'][4]['url']#photo_url
+            print(url_photo)
+            send()
+
+        except:
+            send()
+
+
 
 
         else:
-            print("Нет новых постов")
-            time.sleep(60*4)
+            time.sleep(60)
             main()
 
 
     except:
-        main()
+       main()
+
+
+
+
+
+
+
 
 
 
